feb880eff62e46765df699a9c1f33366d68cd509
Dung Le <dung.le@axonactive.vn>
2016-08-26 11:28:25 +0700

    add start script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"; 
+"use strict";
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
@@ -45,6 +45,10 @@ var PLAYER_ATTRIBUTES = [
     "gkpositioning"
 ];
 
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
 app.get('/scrape', function (req, res) {
     // fs.readFile('player_ids.txt', (err, data) => {
     //   if (err) throw err;
@@ -77,13 +81,12 @@ function getPlayer(playerIds, index, res, rootRef, fs) {
             let player = parsePlayer(html, playerId);
 
             if (player) {
-            	rootRef.child(playerId).set(player).then(function (response) {
-	                next('', playerId, playerIds, index, res, rootRef, fs);
-	            });
+                rootRef.child(playerId).set(player).then(function (response) {
+                    next('', playerId, playerIds, index, res, rootRef, fs);
+                });
             } else {
-            	next(' - Not Found', playerId, playerIds, index, res, rootRef, fs);
+                next(' - Not Found', playerId, playerIds, index, res, rootRef, fs);
             }
-            
 
         } else {
             next(' - Error', playerId, playerIds, index, res, rootRef, fs);
@@ -94,15 +97,14 @@ function getPlayer(playerIds, index, res, rootRef, fs) {
     });
 }
 
-
 function next(type, playerId, playerIds, index, res, rootRef, fs) {
-	fs.appendFileSync("./output.txt", playerId.toString() + type + "\r\n");
+    fs.appendFileSync("./output.txt", playerId.toString() + type + "\r\n");
 
     if (index++ < (playerIds.length - 1)) {
         getPlayer(playerIds, index, res, rootRef, fs);
     } else {
-    	console.log('finished!');
-    	res.send('finished');
+        console.log('finished!');
+        res.send('finished');
     }
 }
 
@@ -110,89 +112,88 @@ function parsePlayer(html, playerId) {
     var $ = cheerio.load(html);
     var player = {pid: playerId};
 
-
     if ($('.stat_list').length > 0) {
-    	// PLAYER_ATTRIBUTES.forEach(function (attr) {
-	    //     var value = $('#display_' + attr).text();
-	    //     player[attr] = value;
-	    // });
+        // PLAYER_ATTRIBUTES.forEach(function (attr) {
+        //     var value = $('#display_' + attr).text();
+        //     player[attr] = value;
+        // });
 
-	    // Get player attributes
-	    $('.stat_list').each(function (i, el) {
-	        let $el = $(el);
-	        let key = $el.attr('class').split(' ')[1];
-	        let value = $el.find('.stat_value').text();
-	        player[key] = value;
-	    });
-	    
-	    let $f3playerTopInfo = $('.f3player_topinfo');
-	    let $nameEl = $f3playerTopInfo.find('.player_info_list.player_name a');
-	    // name
-	    let playerName = $nameEl.text().trim();
-	    let plusIndex = playerName.indexOf("+");
+        // Get player attributes
+        $('.stat_list').each(function (i, el) {
+            let $el = $(el);
+            let key = $el.attr('class').split(' ')[1];
+            let value = $el.find('.stat_value').text();
+            player[key] = value;
+        });
 
-	    if (plusIndex > -1) {
-	    	playerName = playerName.substring(0, plusIndex);
-	    }
+        let $f3playerTopInfo = $('.f3player_topinfo');
+        let $nameEl = $f3playerTopInfo.find('.player_info_list.player_name a');
+        // name
+        let playerName = $nameEl.text().trim();
+        let plusIndex = playerName.indexOf("+");
 
-	    player['fullname'] = playerName;
-	    player['shortname'] = playerName;
+        if (plusIndex > -1) {
+            playerName = playerName.substring(0, plusIndex);
+        }
 
-	    // season
-	    player['badged'] = $nameEl.find('span').attr('class');
+        player['fullname'] = playerName;
+        player['shortname'] = playerName;
 
-	    // positions
-	    let positions = {};
-	    let $positions =  $f3playerTopInfo.find('.player_info_list.player_position');
+        // season
+        player['badged'] = $nameEl.find('span').attr('class');
 
-	    $positions.find('.player_position_list').each(function (i, el) {
-	        let $this = $(el);
-	        positions[$this.find('.badge_position').text()] = $this.find('.stat_value').text();
+        // positions
+        let positions = {};
+        let $positions = $f3playerTopInfo.find('.player_info_list.player_position');
 
-	        if ($this.attr('class').indexOf('player_position_active') > -1) {
-	            player['overallrating'] = $this.find('.stat_value').text();
-	        }
-	    });
+        $positions.find('.player_position_list').each(function (i, el) {
+            let $this = $(el);
+            positions[$this.find('.badge_position').text()] = $this.find('.stat_value').text();
 
-	    player['positions'] = positions;
-	    
-	    // skillmoves
-	    player['skillmoves'] = $f3playerTopInfo.find('.player_info_list.player_skillmoves i').length;
+            if ($this.attr('class').indexOf('player_position_active') > -1) {
+                player['overallrating'] = $this.find('.stat_value').text();
+            }
+        });
 
-	    // nation
-	    player['nation'] = $f3playerTopInfo.find('.player_nation b').text();
-	    // club
-	    player['club'] = $f3playerTopInfo.find('.player_club b').text();
+        player['positions'] = positions;
 
-	    // birthday
-	    let $values = $($f3playerTopInfo.find('.player_info_list')[2]).find('b');
-	    player['birthday'] = $($values[0]).text();
-	    player['height'] = $($values[1]).text();
-	    player['weight'] = $($values[2]).text();
+        // skillmoves
+        player['skillmoves'] = $f3playerTopInfo.find('.player_info_list.player_skillmoves i').length;
 
-	    player['leftfoot'] = $f3playerTopInfo.find('.leftfoot').text().trim();
-	    player['rightfoot'] = $f3playerTopInfo.find('.rightfoot').text().trim();
+        // nation
+        player['nation'] = $f3playerTopInfo.find('.player_nation b').text();
+        // club
+        player['club'] = $f3playerTopInfo.find('.player_club b').text();
 
-	    // attack/defence
-	    let $playerStatInner = $('.player_stat_inner');
-	    player['attack'] = $playerStatInner.find('.workrate .att b').text();
-	    player['defence'] = $playerStatInner.find('.workrate .def b').text();
+        // birthday
+        let $values = $($f3playerTopInfo.find('.player_info_list')[2]).find('b');
+        player['birthday'] = $($values[0]).text();
+        player['height'] = $($values[1]).text();
+        player['weight'] = $($values[2]).text();
 
-	    //speciality
-	    let speciality = [];
-	    $playerStatInner.find('.speciality b').each(function (i, el) {
-	        speciality.push($(el).text());
-	    });
-	    player['speciality'] = speciality;
+        player['leftfoot'] = $f3playerTopInfo.find('.leftfoot').text().trim();
+        player['rightfoot'] = $f3playerTopInfo.find('.rightfoot').text().trim();
 
-	    //trait
-	    let hiddenScore = [];
-	    $($playerStatInner.find('.trait.sm')[1]).find('b').each(function (i, el) {
-	        hiddenScore.push($(el).text());
-	    });
-	    player['hidden_score'] = hiddenScore;
+        // attack/defence
+        let $playerStatInner = $('.player_stat_inner');
+        player['attack'] = $playerStatInner.find('.workrate .att b').text();
+        player['defence'] = $playerStatInner.find('.workrate .def b').text();
+
+        //speciality
+        let speciality = [];
+        $playerStatInner.find('.speciality b').each(function (i, el) {
+            speciality.push($(el).text());
+        });
+        player['speciality'] = speciality;
+
+        //trait
+        let hiddenScore = [];
+        $($playerStatInner.find('.trait.sm')[1]).find('b').each(function (i, el) {
+            hiddenScore.push($(el).text());
+        });
+        player['hidden_score'] = hiddenScore;
     } else {
-		player = undefined;
+        player = undefined;
     }
 
     console.log(player);
